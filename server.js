de4b17913d0e1154adf06b22e9a81b9bc810e9e1
omri yakubovsky <78727629+omriyaku@users.noreply.github.com>
2021-10-26 21:44:55 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+Page up
+  GNU nano 3.2                                                                                      server.js                                                                                                 
+'use strict';
+const express = require('express');
+// Constants
+const PORT = process.env.PORT || 3000;
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
